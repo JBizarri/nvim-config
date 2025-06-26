d716f9f27eb47ad580eb9807afedbe07eff2ca36
Jefferson Bizarri <jefferson.bizarri@gmail.com>
2025-06-26 19:57:17 -0300

    Fix autocomplete

--- lua/plugins/spider.lua
+++ lua/plugins/spider.lua
@@ -3,9 +3,5 @@ return {
   lazy = false,
   config = function()
     require('spider').setup { skipInsignificantPunctuation = false }
-    vim.keymap.set('n', 'c<leader>w', "c<cmd>lua require('spider').motion('e')<CR>", { desc = 'Next subword' })
-    vim.keymap.set({ 'n', 'o', 'x' }, '<leader>w', "<cmd>lua require('spider').motion('w')<CR>", { desc = 'Next subword' })
-    vim.keymap.set({ 'n', 'o', 'x' }, '<leader>e', "<cmd>lua require('spider').motion('e')<CR>", { desc = 'Next end of subword' })
-    vim.keymap.set({ 'n', 'o', 'x' }, '<leader>b', "<cmd>lua require('spider').motion('b')<CR>", { desc = 'Prev word' })
   end,
 }
